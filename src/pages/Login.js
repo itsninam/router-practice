8b0fc7cb1bbefcf276cdf1028e783e25aad9024a
Madalina <madalina@Madalinas-MacBook-Air.local>
2023-09-05 17:16:40 -0400

    Login dummy info

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,14 @@
-import { NavLink } from "react-router-dom";
-import Nav from "../components/Nav";
 import styles from "./Login.module.css";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import Nav from "../components/Nav";
 
 const Login = () => {
+  const [userInput, setUserInput] = useState({
+    email: "johndoe@gmail.com",
+    password: "pass123",
+  });
+
   return (
     <div className={`wrapper ${styles.wrapper}`}>
       <Nav />
@@ -10,10 +16,20 @@ const Login = () => {
       <section className={styles.loginSection}>
         <form action="">
           <label htmlFor="email">Please enter your email</label>
-          <input type="text" id="email" />
+          <input
+            type="text"
+            id="email"
+            value={userInput.email}
+            onChange={(event) => setUserInput(event.target.value)}
+          />
 
           <label htmlFor="password">Please enter your password</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={userInput.password}
+            onChange={(event) => setUserInput(event.target.value)}
+          />
 
           <NavLink to="/app" className={`button ${styles.loginButton}`}>
             Login
